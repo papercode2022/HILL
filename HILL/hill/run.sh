5f6ac46c11082feadfefc740a62243851f1fc4b3
Papercode <weilemeng0717@163.com>
2022-08-29 11:15:48 +0800

    Update run.sh

--- HILL/hill/run.sh
+++ HILL/hill/run.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Ant Maze
-python train_hier_sac.py --c 50 --abs_range 20 --env-name AntMaze1Test-v1 --test AntMaze1Test-v3  --seed 2
+python train_hier_sac.py --c 50 --abs_range 20 --env-name AntMaze1Test-v1 --test AntMaze1Test-v3
 
 ################################################################################################################
 
 # Ant Push
-python train_hier_sac.py --c 50 --abs_range 20 --env-name AntPushTest-v1 --test AntPushTest-v3 --seed 2
+python train_hier_sac.py --c 50 --abs_range 20 --env-name AntPushTest-v1 --test AntPushTest-v3
 
 ################################################################################################################
 
 # Ant FourRoom
-python train_hier_sac.py --c 50 --abs_range 20   --env-name AntMazeTest-v2 --test AntMazeTest-v4 --seed 2
+python train_hier_sac.py --c 50 --abs_range 20   --env-name AntMazeTest-v2 --test AntMazeTest-v4
