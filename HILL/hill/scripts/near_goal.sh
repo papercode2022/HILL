6cac1178bf3fb6f87f38911ffc775486207ada7d
Papercode <weilemeng0717@163.com>
2022-08-29 11:08:10 +0800

    Update near_goal.sh

--- HILL/hill/scripts/near_goal.sh
+++ HILL/hill/scripts/near_goal.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
 
-# ant maze
+# Ant Maze
+python train_hier_sac.py --c 50 --abs_range 20 --env-name AntMaze1Test-v1 --test AntMaze1Test-v3  --seed 2
 
-# single goal, c=50
-python train_hier_sac.py --c 50 --abs_range 20  --env-name AntMaze1Test-v1 --test AntMaze1Test-v1 \
---weight_decay 1e-5 --device cuda:0 --seed 6
+################################################################################################################
 
-# single goal, c=50, image input
-python train_hier_sac.py --c 50 --abs_range 20  --env-name AntMaze1Test-v1 --test AntMaze1Test-v1 \
---image True --weight_decay 1e-5 --device cuda:4 --seed 7
+# Ant Push
+python train_hier_sac.py --c 50 --abs_range 20 --env-name AntPushTest-v1 --test AntPushTest-v3 --seed 2
 
 ################################################################################################################
 
 # Ant FourRoom
-# single goal, c=50
-python train_hier_sac.py --c 50 --abs_range 20  --env-name AntMazeTest-v2 --test AntMazeTest-v2 \
---weight_decay 1e-5 --device cuda:6 --seed 18
-
-# single goal, c=50, image input
-python train_hier_sac.py --c 50 --abs_range 20  --env-name AntMazeTest-v2 --test AntMazeTest-v2 \
---image True --weight_decay 1e-5 --device cuda:6 --seed 13
+python train_hier_sac.py --c 50 --abs_range 20   --env-name AntMazeTest-v2 --test AntMazeTest-v4 --seed 2
